99bf916c1c12eecf1102085495603740c61c2da0
Christian Berndt <berndt@inofix.ch>
2017-04-10 17:24:51 +0200

    Preliminary implementation of the add and update gui.

--- contact-manager-web/src/main/java/ch/inofix/contact/web/internal/portlet/ContactManagerPortlet.java
+++ contact-manager-web/src/main/java/ch/inofix/contact/web/internal/portlet/ContactManagerPortlet.java
@@ -3,6 +3,8 @@ package ch.inofix.contact.web.internal.portlet;
 import java.io.IOException;
 import java.util.Map;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
@@ -19,6 +21,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.ParamUtil;
 
@@ -28,6 +32,10 @@ import ch.inofix.contact.model.Contact;
 import ch.inofix.contact.service.ContactService;
 import ch.inofix.contact.web.configuration.ContactManagerConfiguration;
 import ch.inofix.contact.web.internal.constants.ContactManagerWebKeys;
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.VCardVersion;
+import ezvcard.property.Uid;
 
 /**
  * View Controller of Inofix' timetracker.
@@ -73,6 +81,60 @@ public class ContactManagerPortlet extends MVCPortlet {
         super.render(renderRequest, renderResponse);
     }
 
+    public void updateContact(ActionRequest actionRequest, ActionResponse actionResponse) throws Exception {
+
+        _log.info("updateContact");
+
+        long contactId = ParamUtil.getLong(actionRequest, "contactId");
+
+        ServiceContext serviceContext = ServiceContextFactory.getInstance(Contact.class.getName(), actionRequest);
+
+        Contact contact = null;
+
+        VCard vCard = null;
+        String uid = null;
+
+        if (contactId > 0) {
+
+            contact = _contactService.getContact(contactId);
+            uid = contact.getUid();
+            vCard = contact.getVCard();
+
+        } else {
+
+            vCard = new VCard();
+            vCard.setUid(Uid.random());
+            uid = vCard.getUid().getValue();
+
+        }
+
+        // Store contact information in vCard format
+
+        String card = Ezvcard.write(vCard).version(VCardVersion.V4_0).go();
+
+        _log.info(card);
+
+        if (contactId <= 0) {
+
+            // Add contact
+
+            contact = _contactService.addContact(card, uid, serviceContext);
+
+        } else {
+
+            // Update contact
+
+            contact = _contactService.updateContact(contactId, card, uid, serviceContext);
+        }
+
+        // TODO
+        // String redirect = getEditContactURL(actionRequest, actionResponse,
+        // contact);
+
+        // actionRequest.setAttribute(WebKeys.REDIRECT, redirect);
+
+    }
+
     @Activate
     @Modified
     protected void activate(Map<Object, Object> properties) {
@@ -105,7 +167,7 @@ public class ContactManagerPortlet extends MVCPortlet {
     }
 
     @Reference
-    protected void setTaskRecordService(ContactService contactService) {
+    protected void setContactService(ContactService contactService) {
         this._contactService = contactService;
     }
 
